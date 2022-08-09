6c5859687af70b5ad373263486bc713a98a681c8
Joshua Estes <jne0010@uah.edu>
2022-08-08 23:28:30 -0500

    Added comments and finalizing class implemntation

--- include/auxillary.hpp
+++ include/auxillary.hpp
@@ -1,51 +1,215 @@
 #include <gpiod.h>
 #include <vector>
+#include <string>
+#include <type_traits>
+#include <map>
 
-const char* chipname = "gpiochip0";
-struct gpiod_chip* chip;
-struct gpiod_line* lineRed;    // Red LED
-struct gpiod_line* lineGreen;  // Green LED
-struct gpiod_line* lineYellow; // Yellow LED
-struct gpiod_line* lineButton; // Pushbutton
+#define VERSION 0.1
 
-class init_gpiod {
+#ifndef PROGRAM_NAME
+#define PROGRAM_NAME
+const char *name = "gpio_manipulation_program";
+#endif
+
+struct bug_report
+{
+    const char *err_txt;
+    int err_val;
+};
+
+class init_gpiod
+{
 private:
-    std::vector<struct gpiod_line*> lines;
-    std::vector<struct gpiod_chip*> chips;
+    std::map<std::string, struct gpiod_line *> input_lines;
+    std::map<std::string, struct gpiod_line *> output_lines;
+    std::vector<struct gpiod_chip *> chips;
+
+    std::vector<struct bug_report> errors;
 
     typedef u_int64_t int_t;
 
-public : 
+public:
     init_gpiod() = delete;
 
     /**
-     * @brief Initialize init_gpiod class
-     * @param chip_name 2d char array for all the chip_names being used
-     * @param lines Lines to be retrieved from the chip they are in
-     * @param size sizes for arrays used
-     * @return returns an instance to the class init_gpiod
      *
-     * Initialize lines and chips correctly for GPIO Line manipulation
+     * Initialize lines and chips correctly for GPIO Line manipulation.
      *
-     * size details:
-     *  1st element: amount of chip names
-     *  Nth element: amount of lines per chip correlating to the size of each array inside lines
+     * size details:<
+     *  1st element: amount of chip names.
+     *  Nth element: amount of lines per chip correlating to the size of each array inside lines.
+     *
+     * The lines array is to have two arrays per chip, the first being input and the second being output.
      *
      * example:
      *  chip_name = {"gpiochip0", "gpiochip1" };
-     *  lines = { { 0, 1, 2 }, {0, 3, 5, 6} };
-     *  size = { 2, 3, 4 };
+     *  lines = { { { 0, 1 }, { 2 } }, { { 0, 3 }, { 5, 6 } } };
+     *
+     * @brief Initialize init_gpiod class
+     * @param chip_name 2d char array for all the chip_names being used
+     * @param lines Lines to be retrieved from the chip they are in
+     * @return An instance to the class init_gpiod
+     *
      */
-    init_gpiod(const char **chip_name, int_t **lines_in, int_t *size)
+    init_gpiod(std::vector<const char *> chip_name, std::vector<std::vector<std::vector<int_t>>> lines_in)
     {
-        for (int_t i = 0; i < size[0]; ++i) {
-            chips.push_back(gpiod_chip_open_by_name(chip_name[i]));
+        for (int_t i = 0; i < chip_name.size(); ++i)
+        {
+            auto open_chip_ret_val = gpiod_chip_open_by_name(chip_name[i]);
 
-            if (chips.at(i) == NULL) continue;
+            if (open_chip_ret_val == NULL)
+            {
+                std::string err = "Could not open chip: ";
+                err.append(chip_name[i]);
+                bug_report err_struct = {
+                    .err_txt = err.c_str(),
+                    .err_val = NULL};
 
-            for (int_t j = 0; j < size[j + 1]; ++j) {
-                lines.push_back(gpiod_chip_get_line(chips.at(i), lines_in[i][j]));
+                errors.push_back(err_struct);
+            }
+            else
+            {
+                chips.push_back(open_chip_ret_val);
+            }
+
+            if (chips.at(i) == NULL)
+                continue;
+
+            for (int_t j = 0; j < lines_in.at(i).at(0).size(); ++j)
+            {
+                auto val = gpiod_chip_get_line(chips.at(i), lines_in.at(i).at(0).at(j));
+
+                if (val == NULL)
+                {
+                    std::string err = "Could not get line: " + std::to_string(i);
+                    bug_report err_struct = {
+                        .err_txt = err.c_str(),
+                        .err_val = NULL};
+
+                    errors.push_back(err_struct);
+                }
+                else
+                {
+                    input_lines.push_back(val);
+                }
+            }
+
+            for (int_t j = 0; j < lines_in.at(i).at(1).size(); ++j)
+            {
+                auto val = gpiod_chip_get_line(chips.at(i), lines_in.at(i).at(1).at(j));
+
+                if (val == NULL)
+                {
+                    std::string err = "Could not get line: " + std::to_string(i);
+                    bug_report err_struct = {
+                        .err_txt = err.c_str(),
+                        .err_val = NULL};
+
+                    errors.push_back(err_struct);
+                }
+                else
+                {
+                    output_lines.push_back(val);
+                }
             }
         }
+    }
+
+    /**
+     *
+     * @brief Initialize input lines
+     * @return Amount of errors that occured during function runtime
+     *
+     */
+    int init_input_lines()
+    {
+        int num_of_errors = 0;
+
+        for (int_t i = 0; i < this->input_lines.size(); ++i)
+        {
+            if (gpiod_line_request_input(input_lines.at(i), name) == -1)
+            {
+                bug_report err = {
+                    .err_txt = "Could not reserve line",
+                    .err_val = -1};
+
+                errors.push_back(err);
+                ++num_of_errors;
+            }
+        }
+
+        return num_of_errors;
+    }
+
+    /**
+     *
+     * @brief Initialize output lines
+     * @return Amount of errors that occured during function runtime
+     *
+     */
+    int init_output_lines()
+    {
+        int num_of_errors = 0;
+
+        for (int_t i = 0; i < this->input_lines.size(); ++i)
+        {
+            if (gpiod_line_request_output(input_lines.at(i), name, 0) == -1)
+            {
+                bug_report err = {
+                    .err_txt = "Could not reserve line",
+                    .err_val = -1};
+
+                errors.push_back(err);
+                ++num_of_errors;
+            }
+        }
+
+        return num_of_errors;
+    }
+
+    auto activate_lines(std::vector<std::pair<>>)
+
+        /**
+         *
+         * @brief Returns copy of the errors vector
+         * @return vector of errors, even if empty
+         *
+         */
+        std::vector<struct bug_report> get_errors()
+    {
+        return errors;
+    }
+
+    /**
+     *
+     * @brief Returns copy of the input_lines vector
+     * @return vector of input_lines, even if empty
+     *
+     */
+    std::vector<struct gpiod_line *> get_input_lines()
+    {
+        return input_lines;
+    }
+
+    /**
+     *
+     * @brief Returns copy of the output_lines vector
+     * @return vector of output lines, even if empty
+     *
+     */
+    std::vector<struct gpiod_line *> get_output_lines()
+    {
+        return output_lines;
+    }
+
+    /**
+     *
+     * @brief Returns copy of the chips vector
+     * @return vector of chips, even if empty
+     *
+     */
+    std::vector<struct gpiod_chip *> get_chips()
+    {
+        return chips;
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,93 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <gpiod.h>
+#include <time.h>
 
 #include <chrono>
 #include <thread>
 
-#include "auxillary.hpp"
+// #include "auxillary.hpp"
 
 using namespace std::chrono_literals;
 
+// chip variables
+const char *chipname = "gpiochip0";
+struct gpiod_chip *chip;
+
+// GPIO Lines
+struct gpiod_line *lineRed; // Red LED
+struct gpiod_line *lineBlueOne; // Blue 1 LED
+struct gpiod_line *lineBlueTwo; // Blue 2 LED
+struct gpiod_line *lineBlueThree; // Blue 3 LED
+
+struct gpiod_line *lineButton;
+
+// Time variables
+time_t rawtime;
+struct tm *timeinfo;
+
 int main(int argc, char **argv)
 {
-  int i, val;
+	int i, val;
 
-  // Open GPIO chip
-  chip = gpiod_chip_open_by_name(chipname);
+	// Open GPIO chip
+	chip = gpiod_chip_open_by_name(chipname);
 
-  // Open GPIO lines
-  lineRed = gpiod_chip_get_line(chip, 24);
-  lineGreen = gpiod_chip_get_line(chip, 25);
-  lineYellow = gpiod_chip_get_line(chip, 5);
-  lineButton = gpiod_chip_get_line(chip, 6);
+	// Open GPIO lines
+	lineRed = gpiod_chip_get_line(chip, 2);
+	lineBlueOne = gpiod_chip_get_line(chip, 3);
+	lineBlueTwo = gpiod_chip_get_line(chip, 4);
+	lineBlueThree = gpiod_chip_get_line(chip, 17);
+	lineButton = gpiod_chip_get_line(chip, 15);
 
-  // Open LED lines for output
-  gpiod_line_request_output(lineRed, "example1", 0);
-  gpiod_line_request_output(lineGreen, "example1", 0);
-  gpiod_line_request_output(lineYellow, "example1", 0);
+	// Open LED lines for output
+	if (gpiod_line_request_output(lineRed, "led_enable_test", 0) == -1)
+	{
+		return -1;
+	}
+	if (gpiod_line_request_output(lineBlueOne, "led_enable_test", 0) == -1)
+	{
+		return -1;
+	}
+	if (gpiod_line_request_output(lineBlueTwo, "led_enable_test", 0) == -1)
+	{
+		return -1;
+	}
+	if (gpiod_line_request_output(lineBlueThree, "led_enable_test", 0) == -1)
+	{
+		return -1;
+	}
 
-  // Open switch line for input
-  gpiod_line_request_input(lineButton, "example1");
+	if (gpiod_line_request_input(lineButton, "led_enable_test") == -1)
+	{
+		return -1;
+	}
 
-  // Blink LEDs in a binary pattern
-  i = 0;
-  while (true) {
-    gpiod_line_set_value(lineRed, (i & 1) != 0);
-    gpiod_line_set_value(lineGreen, (i & 2) != 0);
-    gpiod_line_set_value(lineYellow, (i & 4) != 0);
+	// Blink LEDs in a binary pattern
+	for (int i = 0; i < 5; ++i)
+	{
+		gpiod_line_set_value(lineRed, 1);
+		gpiod_line_set_value(lineBlueOne, 0);
+		gpiod_line_set_value(lineBlueTwo, 0);
+		gpiod_line_set_value(lineBlueThree, 0);
+		std::this_thread::sleep_for(1s);
+		gpiod_line_set_value(lineRed, 0);
+		gpiod_line_set_value(lineBlueOne, 1);
+		gpiod_line_set_value(lineBlueTwo, 1);
+		gpiod_line_set_value(lineBlueThree, 1);
+		std::this_thread::sleep_for(1s);
+	}
 
-    // Read button status and exit if pressed
-    val = gpiod_line_get_value(lineButton);
-    if (val == 0) {
-      break;
-    }
+	gpiod_line_set_value(lineRed, 0);
+	gpiod_line_set_value(lineBlueOne, 0);
+	gpiod_line_set_value(lineBlueTwo, 0);
+	gpiod_line_set_value(lineBlueThree, 0);
 
-    std::this_thread::sleep_for(1s);
-
-    usleep(100000);
-    i++;
-  }
-
-  // Release lines and chip
-  gpiod_line_release(lineRed);
-  gpiod_line_release(lineGreen);
-  gpiod_line_release(lineYellow);
-  gpiod_line_release(lineButton);
-  gpiod_chip_close(chip);
-  return 0;
+	// Release lines and chip
+	gpiod_line_release(lineRed);
+	gpiod_line_release(lineBlueOne);
+	gpiod_line_release(lineBlueTwo);
+	gpiod_line_release(lineBlueThree);
+	gpiod_chip_close(chip);
+	return 0;
 }
