86d1fb03962f214f62db9108ef6017448103bad7
Joshua Estes <jne0010@uah.edu>
2022-08-08 23:54:20 -0500

    Added a way to set the values of multiple lines

--- include/auxillary.hpp
+++ include/auxillary.hpp
@@ -4,7 +4,7 @@
 #include <type_traits>
 #include <map>
 
-#define VERSION 0.1
+#define VERSION 0.2
 
 #ifndef PROGRAM_NAME
 #define PROGRAM_NAME
@@ -22,6 +22,10 @@ class init_gpiod
 private:
     std::map<std::string, struct gpiod_line *> input_lines;
     std::map<std::string, struct gpiod_line *> output_lines;
+
+    std::vector<std::string> input_lines_names;
+    std::vector<std::string> output_lines_names;
+
     std::vector<struct gpiod_chip *> chips;
 
     std::vector<struct bug_report> errors;
@@ -42,8 +46,8 @@ public:
      * The lines array is to have two arrays per chip, the first being input and the second being output.
      *
      * example:
-     *  chip_name = {"gpiochip0", "gpiochip1" };
-     *  lines = { { { 0, 1 }, { 2 } }, { { 0, 3 }, { 5, 6 } } };
+     *  chip_name = {"gpiochip0" };
+     *  lines = { { { ("line0", 0),  ("line1", 1) }, { (  "line2", 2) } } };
      *
      * @brief Initialize init_gpiod class
      * @param chip_name 2d char array for all the chip_names being used
@@ -51,7 +55,7 @@ public:
      * @return An instance to the class init_gpiod
      *
      */
-    init_gpiod(std::vector<const char *> chip_name, std::vector<std::vector<std::vector<int_t>>> lines_in)
+    init_gpiod(std::vector<const char *> chip_name, std::vector<std::vector<std::vector<std::pair<std::string, int_t>>>> lines_in)
     {
         for (int_t i = 0; i < chip_name.size(); ++i)
         {
@@ -77,7 +81,7 @@ public:
 
             for (int_t j = 0; j < lines_in.at(i).at(0).size(); ++j)
             {
-                auto val = gpiod_chip_get_line(chips.at(i), lines_in.at(i).at(0).at(j));
+                auto val = gpiod_chip_get_line(chips.at(i), lines_in.at(i).at(0).at(j).second);
 
                 if (val == NULL)
                 {
@@ -90,13 +94,14 @@ public:
                 }
                 else
                 {
-                    input_lines.push_back(val);
+                    input_lines[lines_in.at(i).at(0).at(j).first] = val;
+                    input_lines_names.push_back(lines_in.at(i).at(0).at(j).first);
                 }
             }
 
             for (int_t j = 0; j < lines_in.at(i).at(1).size(); ++j)
             {
-                auto val = gpiod_chip_get_line(chips.at(i), lines_in.at(i).at(1).at(j));
+                auto val = gpiod_chip_get_line(chips.at(i), lines_in.at(i).at(1).at(j).second);
 
                 if (val == NULL)
                 {
@@ -109,7 +114,8 @@ public:
                 }
                 else
                 {
-                    output_lines.push_back(val);
+                    output_lines[lines_in.at(i).at(1).at(j).first] = val;
+                    output_lines_names.push_back(lines_in.at(i).at(1).at(j).first);
                 }
             }
         }
@@ -127,7 +133,7 @@ public:
 
         for (int_t i = 0; i < this->input_lines.size(); ++i)
         {
-            if (gpiod_line_request_input(input_lines.at(i), name) == -1)
+            if (gpiod_line_request_input(input_lines[input_lines_names.at(i)], name) == -1)
             {
                 bug_report err = {
                     .err_txt = "Could not reserve line",
@@ -151,9 +157,9 @@ public:
     {
         int num_of_errors = 0;
 
-        for (int_t i = 0; i < this->input_lines.size(); ++i)
+        for (int_t i = 0; i < this->input_lines_names.size(); ++i)
         {
-            if (gpiod_line_request_output(input_lines.at(i), name, 0) == -1)
+            if (gpiod_line_request_output(output_lines[output_lines_names.at(i)], name, 0) == -1)
             {
                 bug_report err = {
                     .err_txt = "Could not reserve line",
@@ -167,39 +173,65 @@ public:
         return num_of_errors;
     }
 
-    auto activate_lines(std::vector<std::pair<>>)
+    /**
+     *
+     * @brief Initialize output lines
+     * @param lines a vector of pairs that include the name and value to set that line to
+     * @return Amount of errors that occured during function runtime
+     *
+     */
+    int set_lines(std::vector<std::pair<std::string, int_t>> lines)
+    {
+        int num_of_errors = 0;
+        for (auto [name, value] : lines)
+        {
+            if (gpiod_line_set_value(output_lines[name], value) == -1)
+            {
+                std::string err_str = "Could not set line " + name;
 
-        /**
-         *
-         * @brief Returns copy of the errors vector
-         * @return vector of errors, even if empty
-         *
-         */
-        std::vector<struct bug_report> get_errors()
+                bug_report err = {
+                    .err_txt = err_str.c_str(),
+                    .err_val = -1};
+
+                errors.push_back(err);
+                ++num_of_errors;
+            }
+        }
+
+        return num_of_errors;
+    }
+
+    /**
+     *
+     * @brief Returns copy of the errors vector
+     * @return vector of errors, even if empty
+     *
+     */
+    std::vector<struct bug_report> get_errors()
     {
         return errors;
     }
 
     /**
      *
-     * @brief Returns copy of the input_lines vector
-     * @return vector of input_lines, even if empty
+     * @brief Returns copy of the input_lines_names vector
+     * @return vector of input_lines_names, even if empty
      *
      */
-    std::vector<struct gpiod_line *> get_input_lines()
+    std::vector<std::string> get_input_lines()
     {
-        return input_lines;
+        return input_lines_names;
     }
 
     /**
      *
-     * @brief Returns copy of the output_lines vector
-     * @return vector of output lines, even if empty
+     * @brief Returns copy of the output_lines_names vector
+     * @return vector of output lines_names, even if empty
      *
      */
-    std::vector<struct gpiod_line *> get_output_lines()
+    std::vector<std::string> get_output_lines()
     {
-        return output_lines;
+        return output_lines_names;
     }
 
     /**
